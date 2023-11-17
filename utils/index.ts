b8e08ca0fe484e3ff92a596cd653596109b13171
rogervalentim <valentim.roger33@gmail.com>
2023-11-17 00:19:36 -0300

    fix: enviroment variable

--- utils/index.ts
+++ utils/index.ts
@@ -18,13 +18,16 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
 export async function fetchCars(filters: FilterProps) {
     const { manufacturer, year, model, limit, fuel} = filters
     const headers = {
-        'X-RapidAPI-Key': process.env.RAPID_API_KEY,
-		'X-RapidAPI-Host': process.env.RAPID_API_HOST
+        'X-RapidAPI-Key': process.env.RAPID_API_KEY || '',
+		'X-RapidAPI-Host': process.env.RAPID_API_HOST || ''
     }
 
-    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {
-        headers: headers,
-    })
+    const response = await fetch(
+        `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
+        {
+          headers: headers,
+        }
+      );
 
     const result = await response.json()
 
